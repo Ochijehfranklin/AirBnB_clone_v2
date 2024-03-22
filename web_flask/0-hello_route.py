78e9bf1c8bfcd1b2d20118d27ea4fed6e731e19d
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-03-22 14:14:02 -0700

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 def hello_hbnb():
     """prints message when slash is called"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Main Function"""
     app.run(host='0.0.0.0', port=5000)
 
